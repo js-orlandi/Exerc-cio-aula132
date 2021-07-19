2ae31bff6c27de371fa2eac9a2a12666eef668f6
Juliano S Orlandi <js.orlandi.dev@gmail.com>
2021-07-19 20:56:00 -0300

    Adicionar arquivos de projeto.

---
+++ Exercício resolvido - PARTE 2/Entities/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_resolvido___PARTE_2.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double valuePerHour { get; set; }
+
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            this.valuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return Hours * valuePerHour;
+        }
+    }
+}
